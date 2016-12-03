1e485ddb19026f9ab5b99cb550a85b6cc4370fcd
ducphucle <leducphuc1994@gmail.com>
2016-12-03 15:36:58 +0700

    chapter 1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import KanbanBoard from './KanbanBoard';
+ 
+let cardsList = [
+{
+id: 1,
+title: "Read the Book",
+description: "I should read the whole book",
+status: "in-progress",
+tasks: []
+},
+{
+id: 2,
+title: "Write some code",
+description: "Code along with the samples in the book",
+status: "todo",
+tasks: [
+{
+id: 1,
+name: "ContactList Example",
+done: true
+},
+{
+id: 2,
+name: "Kanban Example",
+done: false
+},
+{
+id: 3,
+name: "My own experiments",
+done: false
+}
+]
+},
+];
+ 
+ReactDOM.render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
